ca949f39b68a974cb4f7ad44643734c2d65e98ce
Iamwithcode <micj7961@gmail.com>
2026-06-21 16:37:50 +0530

    Checkpoint 4 - ExecutorService thread pool added

---
+++ src/main/java/com/multi/async_report_service/config/ThreadPoolConfig.java
@@ -0,0 +1,22 @@
+package com.multi.async_report_service.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+@Configuration
+public class ThreadPoolConfig {
+
+
+    @Bean
+    public ExecutorService taskExecutor() {
+
+        return Executors.newFixedThreadPool(4);
+
+    }
+
+}
--- src/main/java/com/multi/async_report_service/service/JobService.java
+++ src/main/java/com/multi/async_report_service/service/JobService.java
@@ -1,10 +1,13 @@
 package com.multi.async_report_service.service;
 
+
 import com.multi.async_report_service.entity.Job;
 import com.multi.async_report_service.repository.JobRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+
 
 
 @Service
@@ -13,10 +16,20 @@ public class JobService {
 
     private final JobRepository jobRepository;
 
+    private final ExecutorService executorService;
 
-    public JobService(JobRepository jobRepository) {
+
+
+    public JobService(
+            JobRepository jobRepository,
+            ExecutorService executorService
+    ) {
+
         this.jobRepository = jobRepository;
+        this.executorService = executorService;
+
     }
+
 
 
     public Job createJob() {
@@ -30,6 +43,52 @@ public class JobService {
         job.setCreatedTime(LocalDateTime.now());
 
 
-        return jobRepository.save(job);
+        Job savedJob = jobRepository.save(job);
+
+
+
+        executorService.submit(() -> {
+
+
+            processJob(savedJob);
+
+
+        });
+
+
+
+        return savedJob;
+
     }
+
+
+
+    private void processJob(Job job) {
+
+
+        System.out.println(
+                "Processing started by : "
+                        + Thread.currentThread().getName()
+        );
+
+
+        try {
+
+            Thread.sleep(10000);
+
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        }
+
+
+        System.out.println(
+                "Processing completed by : "
+                        + Thread.currentThread().getName()
+        );
+
+    }
+
 }
