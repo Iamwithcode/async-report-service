489fd8ac2b0dbf9f32020b74c8319470e4f007af
Iamwithcode <micj7961@gmail.com>
2026-06-21 15:56:40 +0530

    Checkpoint 1 - Initial Spring Boot microservice setup

---
+++ src/main/java/com/multi/async_report_service/AsyncReportServiceApplication.java
@@ -0,0 +1,16 @@
+package com.multi.async_report_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncReportServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(
+				AsyncReportServiceApplication.class,
+				args
+		);
+	}
+}
